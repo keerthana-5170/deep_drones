c6e400245b8b09c613d017ca3aa5fc995a6c5d8f
githubkeerthana <keerthana.sundarbabu@gmail.com>
2021-07-22 12:07:34 +0530

    cart changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,71 @@
 let carts = document.querySelectorAll('.add-cart');
 
+let products = [
+    {
+        name:'drone1',
+        price:20,
+        incart:0
+    },
+    {
+        name:'drone2',
+        price:40,
+        incart:0
+    },
+    {
+        name:'drone3',
+        price:60,
+        incart:0
+    },
+    {
+        name:'drone4',
+        price:20,
+        incart:0
+    },
+    {
+        name:'drone5',
+        price:70,
+        incart:0
+    },
+    {
+        name:'drone6',
+        price:40,
+        incart:0
+    }
+
+]
+
+
+
+
 for(let i=0;i<carts.length;i++){
-    console.log("my loop");
+    carts[i].addEventListener('click',() => {
+        cartNumbers();
+    })
 }
+
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if(productNumbers)
+    {
+        localStorage.setItem('cartNumbers',productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    }
+    
+    else{
+        localStorage.setItem('cartNumbers',1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+
+}
+onLoadCartNumbers();
